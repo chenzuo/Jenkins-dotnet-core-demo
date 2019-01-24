ff933ac467126f109d37ce71cc2b88566529ca68
abcd@1234 <chenzuo@hotmail.com>
2019-01-24 17:13:15 +0800

    add descript

--- Jenkins-dotnet-core-demo/XUnitTestProject1/UnitTest1.cs
+++ Jenkins-dotnet-core-demo/XUnitTestProject1/UnitTest1.cs
@@ -3,14 +3,22 @@ using Xunit;
 
 namespace XUnitTestProject1
 {
+    /// <summary>
+    /// 测试方法
+    /// </summary>
     public class UnitTest1
     {
+        /// <summary>
+        /// 测试方法一
+        /// </summary>
         [Fact]
         public void Test1()
         {
             ClassLibrary1.Class1 class1 = new ClassLibrary1.Class1();
             var plus = class1.Plus(1, 2);
             Assert.Equal(3, plus);
+            plus = class1.Plus(1, 3);
+            Assert.Equal(4, plus);
         }
     }
 }
