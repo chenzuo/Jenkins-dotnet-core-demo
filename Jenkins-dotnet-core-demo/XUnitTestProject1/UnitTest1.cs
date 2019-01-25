94111f961f520f1d8a011346e6fb031ed1f1f817
abcd@1234 <chenzuo@hotmail.com>
2019-01-25 18:46:15 +0800

    add muest

--- Jenkins-dotnet-core-demo/XUnitTestProject1/UnitTest1.cs
+++ Jenkins-dotnet-core-demo/XUnitTestProject1/UnitTest1.cs
@@ -20,5 +20,17 @@ namespace XUnitTestProject1
             plus = class1.Plus(1, 3);
             Assert.Equal(4, plus);
         }
+        /// <summary>
+        /// 增加测试方法二
+        /// </summary>
+        [Fact]
+        public void Test2()
+        {
+            ClassLibrary1.Class1 class1 = new ClassLibrary1.Class1();
+            var plus = class1.Plus(2, 2);
+            Assert.Equal(4, plus);
+            plus = class1.Plus(3, 3);
+            Assert.Equal(6, plus);
+        }
     }
 }
